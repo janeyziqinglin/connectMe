2d7de0a25e9d5485272fb2606fe3a284b549c5cb
Janeyziqinglin <janeyziqinglin@gmail.com>
2023-03-27 14:55:30 -0400

    include backend

---
+++ connectme-back/src/main/java/com/codewithjaney/ConnectMe/ConnectMeApplication.java
@@ -0,0 +1,14 @@
+package com.codewithjaney.ConnectMe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectMeApplication.class, args);
+	}
+}
+
+
